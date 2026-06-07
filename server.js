31cfd679d1efc29fbf81a769bf8049115b65bde5
nawarajwork-cmd <nawaraj.work@gmail.com>
2026-06-07 22:37:43 +0545

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ' https://nawarajwork-cmd.github.io',
+    origin: 'https://nawarajwork-cmd.github.io',
     credentials: true
 }));
 
